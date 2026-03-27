ed27a5d937b9b5f1bf8f0f7934160da8142502f3
Guilherme Pires <guisantospires.238@gmail.com>
2026-03-27 00:46:05 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,22 @@
+import csv
+
+total = 0
+produtos = {}
+
+with open('dados.csv', newline='', encoding= 'utf-8') as arquivo:
+    leitor = csv.DictReader(arquivo)
+    for linha in leitor:
+        produto = linha['produto']
+        preco = int(linha['preco'])
+        quantidade = int(linha['quantidade'])
+        total += preco * quantidade
+        print(produto,'|qnt:',quantidade,'|R$:', preco)
+        if produto in produtos:
+            produtos[produto] += quantidade
+        else:
+            produtos[produto] = quantidade
+print('\nTodos de vendas: ', total)
+
+print('\nTodos os produtos: ')
+for p, q in produtos.items():
+    print(p, 'qnt:',q)
